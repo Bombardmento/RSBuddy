7ab4bf8a5eb346cec1a2c4c058ee82babb5b6c7a
Bombardmento <46rajan@gmail.com>
2013-05-20 14:27:21 -0700

    First upload

---
+++ Tutorial Part 1/classes/tutorial.java
@@ -0,0 +1,19 @@
+package classes;/*
+@author: Bombardmento
+@platform: RSBuddy
+*/
+
+import com.rsbuddy.api.gui.Location;
+import com.rsbuddy.plugin.WidgetPluginBase;
+import javafx.scene.Node;
+
+public class tutorial extends WidgetPluginBase{
+    @Override
+    public Node content(Location location) {
+        return null;
+    }
+
+    public tutorial () {
+        super ("Tutorial","","","");
+    }
+}
